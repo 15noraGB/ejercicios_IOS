3005a5441aa1b7fc7b9babf586756348d6618f1d
Student <noratiangb@gmail.com>
2025-09-30 17:17:59 +0200

    completado ejercicio enums

--- SwiftStepByStep.playground/Pages/10 Enums.xcplaygroundpage/Contents.swift
+++ SwiftStepByStep.playground/Pages/10 Enums.xcplaygroundpage/Contents.swift
@@ -12,6 +12,7 @@ enum NetworkState {
     case loading
     case success(data: String)
     case failure(code: Int, message: String)
+    case redirect(url: String)
 }
 
 // Función que recibe un NetworkState y muestra por pantalla un mensaje distinto
@@ -26,6 +27,8 @@ func render(_ state: NetworkState) {
         print("Success with data:", data) // Mostramos los datos recibidos
     case .failure(let code, let message):
         print("Error \(code):", message) // Mostramos código y mensaje de error
+    case .redirect(url: let url):
+        print("Redirected to:", url) 
     }
 }
 
